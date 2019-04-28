66c39342478775cf0da49ecf58c3694b6a518cf0
lq@jianzhimao.com <1281058343@qq.com>
2019-04-28 17:55:46 +0800

    添加空项目

---
+++ src/test/java/awesome/data/structure/AwesomeDataStructureApplicationTests.java
@@ -0,0 +1,16 @@
+package awesome.data.structure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwesomeDataStructureApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
